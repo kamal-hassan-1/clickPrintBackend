8305e4c1739a6082e9ba4256370c9e7daea172c8
Abdul Ahad <46815089+ahad19n@users.noreply.github.com>
2026-07-05 12:42:23 +0500

    updates

--- src/models/History.js
+++ src/models/History.js
@@ -2,4 +2,8 @@ const mongoose = require('mongoose');
 const Job = require('./Job');
 
 const historySchema = Job.jobSchema.clone();
-module.exports = mongoose.model('History', historySchema);
+
+const History = mongoose.model('History', historySchema);
+History.historyPopulate = Job.jobPopulate;
+
+module.exports = History;
